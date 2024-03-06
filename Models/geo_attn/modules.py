af123a67f1f8dfc3e867446360162c3598fa2ca7
shangjiedu <sd3497@columbia.edu>
2024-03-05 23:04:59 -0800

    update models

---
+++ Models/geo_attn/modules.py
@@ -0,0 +1,269 @@
+import torch
+import torch.nn as nn
+
+from einops import rearrange
+
+
+
+class Mlp(nn.Module):
+    def __init__(
+            self,
+            in_features,
+            hidden_features=None,
+            out_features=None,
+            act_layer=nn.GELU,
+            norm_layer=None,
+            bias=True,
+            drop=0.,
+    ):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+
+        self.fc1 = nn.Linear(in_features, hidden_features, bias=bias)
+        self.act = act_layer()
+        self.drop1 = nn.Dropout(drop)
+        self.norm = norm_layer(hidden_features) if norm_layer is not None else nn.Identity()
+        self.fc2 = nn.Linear(hidden_features, out_features, bias=bias)
+        self.drop2 = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x)
+        x = self.norm(x)
+        x = self.fc2(x)
+        x = self.drop2(x)
+        return x
+    
+
+class loc_embed(nn.Module):
+    def __init__(self, input_dim=2, embed_dim=64):
+        super(loc_embed, self).__init__()
+        self.embed = Mlp(in_features=input_dim,
+                         hidden_features=embed_dim,
+                         out_features=embed_dim,
+                         act_layer=nn.GELU,
+                         norm_layer=nn.BatchNorm1d,
+                         drop=0.0)
+
+    def forward(self, x):
+        return self.embed(x)
+    
+    
+class ser_embed(nn.Module):
+    def __init__(self, in_channel=1, out_channel=64):
+        super(ser_embed, self).__init__()
+        self.embed = nn.Sequential(
+            nn.Conv1d(in_channel, out_channel, kernel_size=24, stride=1, padding=0),
+            nn.GELU(),
+            nn.BatchNorm1d(out_channel),
+            nn.Conv1d(out_channel, out_channel, kernel_size=1, stride=1, padding=0),
+            nn.GELU(),
+            nn.BatchNorm1d(out_channel)
+        )
+
+    def forward(self, x):
+        return self.embed(x)
+    
+
+class Embedding(nn.Module):
+    def __init__(self, loc_dim=2, ser_dim=1, loc_embed_dim=64, ser_embed_dim=64):
+        super(Embedding, self).__init__()
+        self.loc_embed = loc_embed(loc_dim, loc_embed_dim)
+        self.ser_embed = ser_embed(ser_dim, ser_embed_dim)
+
+    def forward(self, x):
+        """
+        Input:
+            x: A tuple of 3 tensors.
+               The first tensor has shape (batch_size, n_sensors, 2) and contains the longitude and latitude of monitored sensors.
+               The second tensor has shape (batch_size, n_steps, n_sensors) and contains the pm25 readings of monitored sensors. n_steps should be 24.
+               The third tensor has shape (batch_size, 2) and contains the longitude and latitude of the target sensor.
+        Output:
+            A tuple of 3 tensors.
+            The first tensor has shape (batch_size, n_sensors, loc_embed_dim) and contains the embedded location of monitored sensors.
+            The second tensor has shape (batch_size, ser_embed_dim, n_sensors) and contains the embedded pm25 readings of monitored sensors.
+            The third tensor has shape (batch_size, loc_embed_dim) and contains the embedded location of the target sensor.
+        """
+        loc, ser, target_loc = x
+        batch_size, n_steps, n_sensors = ser.shape
+        
+        # embed location
+        loc = rearrange(loc, 'b n d -> (b n) d', b=batch_size)   # (batch_size * n_sensors, 2)
+        loc_embed = self.loc_embed(loc)   # (batch_size * n_sensors, loc_embed_dim)
+        loc_embed = rearrange(loc_embed, '(b n) d -> b n d', b=batch_size)   # (batch_size, n_sensors, loc_embed_dim)
+        target_loc_embed = self.loc_embed(target_loc)   # (batch_size, loc_embed_dim)
+
+        # embed pm25 readings
+        ser = rearrange(ser, 'b l n -> (b n) l').unsqueeze(1)   # (batch_size * n_sensors, 1, n_steps)
+        ser_embed = self.ser_embed(ser).squeeze(-1)   # (batch_size * n_sensors, ser_embed_dim)
+        ser_embed = rearrange(ser_embed, '(b n) d -> b n d', b=batch_size)   # (batch_size, n_sensors, ser_embed_dim)
+
+        return loc_embed, ser_embed, target_loc_embed
+    
+
+class Cross_Attention(nn.Module):
+    def __init__(self,
+                 dim=64,
+                 num_heads=8,
+                 qkv_bias=False,
+                 qk_norm=False,
+                 attn_trop=0.,
+                 proj_drop=0.,
+                 norm_layer=nn.LayerNorm):
+        super(Cross_Attention, self).__init__()
+        super().__init__()
+        assert dim % num_heads == 0, 'dim should be divisible by num_heads'
+        self.num_heads = num_heads
+        self.head_dim = dim // num_heads
+        self.scale = self.head_dim ** -0.5
+        
+        self.q = nn.Linear(dim, dim, bias=qkv_bias)
+        self.k = nn.Linear(dim, dim, bias=qkv_bias)
+        self.v = nn.Linear(dim, dim, bias=qkv_bias)
+        self.q_norm = norm_layer(dim) if qk_norm else nn.Identity()
+        self.k_norm = norm_layer(dim) if qk_norm else nn.Identity()
+
+        self.attn_drop = nn.Dropout(attn_trop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+    
+    def forward(self, x1, x2):
+        assert x1.shape == x2.shape, 'The shape of x1 and x2 should be the same.'
+        B, N, D = x1.shape
+
+        # q, k, v: (B, num_heads, N, head_dim)
+        q = self.q_norm(self.q(x1)).reshape(B, N, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
+        k = self.k_norm(self.k(x2)).reshape(B, N, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
+        v = self.v(x2).reshape(B, N, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
+        
+        # attn: (B, num_heads, N, N)
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+        
+        # x: (B, num_heads, N, head_dim) -> (B, N, num_heads, head_dim) -> (B, N, D)
+        x = (attn @ v).transpose(1, 2).reshape(B, N, D)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+
+        return x
+    
+
+class Cross_Attention_Block(nn.Module):
+    def __init__(self,
+                 dim=64,
+                 num_heads=8,
+                 qkv_bias=False,
+                 qk_norm=False,
+                 proj_drop=0.,
+                 attn_drop=0.,
+                 norm_layer=nn.LayerNorm,
+                 act_layer=nn.GELU,
+                 mlp_layer=Mlp):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = Cross_Attention(
+            dim=dim,
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            qk_norm=qk_norm,
+            attn_trop=attn_drop,
+            proj_drop=proj_drop,
+            norm_layer=norm_layer
+        )
+        
+        self.norm2 = norm_layer(dim)
+        self.mlp = mlp_layer(in_features=dim, hidden_features=dim*4, out_features=dim,
+                             act_layer=act_layer, norm_layer=norm_layer, drop=proj_drop)
+        
+    def forward(self, x1, x2):
+        x = self.attn(self.norm1(x1), self.norm1(x2))
+        x = x + x1
+        x = x + self.mlp(self.norm2(x))
+        return x
+
+
+class Dual_Cross_Attention_Block(nn.Module):
+    def __init__(self,
+                 dim=64,
+                 num_heads=8,
+                 qkv_bias=False,
+                 qk_norm=False,
+                 proj_drop=0.,
+                 attn_drop=0.,
+                 norm_layer=nn.LayerNorm,
+                 act_layer=nn.GELU,
+                 mlp_layer=Mlp):
+        super().__init__()
+        self.cab1 = Cross_Attention_Block(
+            dim=dim,
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            qk_norm=qk_norm,
+            proj_drop=proj_drop,
+            attn_drop=attn_drop,
+            norm_layer=norm_layer,
+            act_layer=act_layer,
+            mlp_layer=mlp_layer
+        )
+        self.cab2 = Cross_Attention_Block(
+            dim=dim,
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            qk_norm=qk_norm,
+            proj_drop=proj_drop,
+            attn_drop=attn_drop,
+            norm_layer=norm_layer,
+            act_layer=act_layer,
+            mlp_layer=mlp_layer
+        )
+
+    def forward(self, x1, x2):
+        x1 = self.cab1(x1, x2)
+        x2 = self.cab2(x2, x1)
+        return x1, x2
+    
+
+class Geo_Attention_Model(nn.Module):
+    def __init__(self, loc_dim=2, ser_dim=1, loc_embed_dim=64, ser_embed_dim=64, num_blocks=3,
+                 dim=64, num_heads=8, qkv_bias=False, qk_norm=False, proj_drop=0., attn_drop=0.,
+                 norm_layer=nn.LayerNorm, act_layer=nn.GELU, mlp_layer=Mlp):
+        super(Geo_Attention_Model, self).__init__()
+        self.embedding = Embedding(loc_dim, ser_dim, loc_embed_dim, ser_embed_dim)
+
+        self.pred_token = nn.Parameter(torch.randn(1, 1, dim))
+
+        self.blocks = nn.ModuleList([
+            Dual_Cross_Attention_Block(
+                dim=dim,
+                num_heads=num_heads,
+                qkv_bias=qkv_bias,
+                qk_norm=qk_norm,
+                proj_drop=proj_drop,
+                attn_drop=attn_drop,
+                norm_layer=norm_layer,
+                act_layer=act_layer,
+                mlp_layer=mlp_layer
+            ) for _ in range(num_blocks)
+        ])
+
+    def forward(self, x):
+        # loc_embed: (batch_size, n_sensors, dim)
+        # ser_embed: (batch_size, n_sensors, dim)
+        # target_loc_embed: (batch_size, dim)
+        loc_embed, ser_embed, target_loc_embed = self.embedding(x)
+        B, N, D = loc_embed.shape
+
+        # concatenate target and monitored sensors
+        loc = torch.cat((loc_embed, target_loc_embed.unsqueeze(1)), dim=1)
+        ser = torch.cat((ser_embed, self.pred_token.expand(B, 1, D)), dim=1)
+
+        # dual cross attention
+        for block in self.blocks:
+            loc, ser = block(loc, ser)
+
+        return loc, ser
+
+        
---
+++ Models/utils/data.py
@@ -0,0 +1,123 @@
+import os
+import torch
+import pandas as pd
+import numpy as np
+
+
+def average_hour(df, columns=["longitude", "latitude", "pm25"]):
+    """
+    Set negative values to zero and average readings for each hour
+    Input:
+        df: a dataframe with a column named "timestamp" and another column named "pm25
+        columns: a list of columns to be averaged
+    Ouput:
+        df: a dataframe with averaged pm25 readings for each hour
+    """
+
+    # decompose timestamp
+    df['timestamp'] = pd.to_datetime(df['timestamp'], format='mixed')
+    df['year'] = df['timestamp'].dt.year
+    df['month'] = df['timestamp'].dt.month
+    df['day'] = df['timestamp'].dt.day
+    df['hour'] = df['timestamp'].dt.hour
+    df['weekday'] = df['timestamp'].dt.weekday
+
+    # set negative pm25 to 0
+    df.loc[df["pm25"] < 0, "pm25"] = 0
+
+    # averaging
+    df = df.loc[:, columns + ["year", "month", "day", "hour", "weekday"]]
+    df = df.groupby(["year", "month", "day", "hour", "weekday"]).mean().reset_index(drop=False)
+    
+    return df
+
+def load_data(train_ratio=0.7, val_ratio=0.1, test_ratio=0.2, seed=42):
+    data_dir = '/Users/shangjiedu/Desktop/aJay/Merced/Research/Air Quality/InterpolationBaseline/data/Oct0123_Jan3024/'
+    files = [f for f in os.listdir(data_dir) if f.endswith('.csv')]
+    data = []
+    for i, file in enumerate(files):
+        df = pd.read_csv(data_dir + file)
+        df = average_hour(df)
+
+        # remove sensors with missing data
+        if len(df) < 2928:
+            print("File{}: {} contains missing hours".format(i, file))
+            continue
+
+        # remove sensors with outliers
+        if df["pm25"].max() > 500:
+            print("File{}: {} contains outliers".format(i, file))
+            continue
+
+        data.append(df.loc[:, ['pm25', 'longitude', 'latitude']])
+
+    data = np.array(data).transpose(1, 0, 2)
+    
+    np.random.seed(seed)
+    perm = np.random.permutation(data.shape[1])
+    n_train = int(train_ratio * data.shape[1])
+    n_val = int(val_ratio * data.shape[1])
+    n_test = int(test_ratio * data.shape[1])
+    train_idx = perm[:n_train]
+    val_idx = perm[n_train: n_train + n_val]
+    test_idx = perm[n_train + n_val:]
+    idx_list = [train_idx, val_idx, test_idx]
+
+    train_dataset = AirQualityDataset(data, idx_list=idx_list, dataset_type="train")
+    val_dataset = AirQualityDataset(data, idx_list=idx_list, dataset_type="val")
+    test_dataset = AirQualityDataset(data, idx_list=idx_list, dataset_type="test")
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32, shuffle=False)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=False)
+    return train_loader, val_loader, test_loader
+
+class AirQualityDataset(torch.utils.data.Dataset):
+    def __init__(self, data, window=24, idx_list=None, dataset_type="train"):
+        """
+        Input:
+            data: a 3D numpy array with shape (n_steps, n_sensors, 3), where the last dimension contains pm25 readings, longitude and latitude
+            window: the number of lookback hours
+        """
+        self.readings = torch.from_numpy(data[:, :, 0]).float()   # (n_steps, n_sensors)
+
+        self.locations = torch.from_numpy(data[0, :, 1:]).float()   # (n_sensors, 2)
+        self.locations[:, 0] /= 180
+        self.locations[:, 1] /= 90
+
+        self.window = window
+        self.train_idx = idx_list[0]
+        self.val_idx = idx_list[1]
+        self.test_idx = idx_list[2]
+        self.type= dataset_type
+
+    def __len__(self):
+        if self.type == "train":
+            return (self.readings.shape[0] - self.window + 1) * len(self.train_idx)
+        elif self.type == "val":
+            return (self.readings.shape[0] - self.window + 1) * len(self.val_idx)
+        else:
+            return (self.readings.shape[0] - self.window + 1) * len(self.test_idx)
+    def __getitem__(self, idx):
+        if self.type == "train":
+            target_idx = self.train_idx[idx % len(self.train_idx)]
+            monitored_idx = np.concatenate([self.train_idx[: idx % len(self.train_idx)], self.train_idx[idx % len(self.train_idx) + 1:]])
+            start = idx // len(self.train_idx)
+            end = start + self.window
+        elif self.type == "val":
+            target_idx = self.val_idx[idx % len(self.val_idx)]
+            monitored_idx = self.train_idx
+            start = idx // len(self.val_idx)
+            end = start + self.window
+        else:
+            target_idx = self.test_idx[idx % len(self.test_idx)]
+            monitored_idx = self.train_idx
+            start = idx // len(self.test_idx)
+            end = start + self.window
+
+        target_location = self.locations[target_idx, :]
+        target_reading = self.readings[start:end, target_idx]
+        monitored_locations = self.locations[monitored_idx, :]
+        monitored_readings = self.readings[start:end, monitored_idx]
+
+        return monitored_locations, monitored_readings, target_location, target_reading
