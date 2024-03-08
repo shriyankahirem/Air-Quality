8186de91c95a4a56e0747a1dcc5fcfbd268aade0
Shangjie Du <sd3497@columbia.edu>
2024-03-07 22:41:17 -0800

    update tool

--- Models/utils/tool.py
+++ Models/utils/tool.py
@@ -1,3 +1,8 @@
 def count_params(model):
     num_params = sum([p.numel() for p in model.parameters() if p.requires_grad])
     return num_params
+
+def model_size(model):
+    """Calculate the size of the model in mb, given the number of parameters"""
+    num_params = count_params(model)
+    return num_params * 4 / 1024 / 1024
